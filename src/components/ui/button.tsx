1a231796fc425916d7fc627eeccb1df702dda795
Nguyễn Đình Tuân <tuanndph33203@fpt.edu.vn>
2024-07-03 02:02:23 +0700

    new ProductCard component and config ui button

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -19,13 +19,14 @@ const buttonVariants = cva(
           'bg-slate-100 text-slate-900 hover:bg-slate-100/80 dark:bg-slate-800 dark:text-slate-50 dark:hover:bg-slate-800/80',
         ghost: 'hover:bg-slate-100 hover:text-slate-900 dark:hover:bg-slate-800 dark:hover:text-slate-50',
         link: 'text-slate-900 underline-offset-4 hover:underline dark:text-slate-50',
-        auth: 'bg-black rounded-[8px] button-s text-white'
+        auth: 'bg-black rounded-[8px] button-s text-white',
+        icon: 'rounded-full flex items-center justify-center bg-white border shadow-md p-2 transform translate-y-10 group-hover:translate-y-0 transition-all duration-500 ease-in-out opacity-0 group-hover:opacity-100 hover:scale-110'
       },
       size: {
         default: 'h-10 px-4 py-2',
         sm: 'h-9 rounded-md px-3',
         lg: 'h-11 rounded-md px-8',
-        icon: 'h-10 w-10',
+        icon: 'h-12 w-12',
         auth: 'w-full py-3 sm:py-4'
       }
     },
