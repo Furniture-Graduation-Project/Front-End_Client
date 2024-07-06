8453c5188f8fb74bfa079a7862f5cb8f9488386f
Nguyễn Đình Tuân <tuanndph33203@fpt.edu.vn>
2024-07-06 13:55:59 +0700

    Translate the registration page and login page

--- src/pages/(auth)/SignUp.tsx
+++ src/pages/(auth)/SignUp.tsx
@@ -8,6 +8,7 @@ import { Input } from '@/components/ui/input'
 import { useState } from 'react'
 import { Eye, EyeOff } from 'lucide-react'
 import { Link } from 'react-router-dom'
+import { useTranslate } from '@/hooks/useTranslate'
 
 const FormSchema = z.object({
   yourname: z.string().min(3, {
@@ -24,7 +25,9 @@ const FormSchema = z.object({
   }),
   remember: z.boolean()
 })
+
 const SignUp = () => {
+  const { t } = useTranslate('signup')
   const form = useForm<z.infer<typeof FormSchema>>({
     resolver: zodResolver(FormSchema),
     defaultValues: {
@@ -40,14 +43,15 @@ const SignUp = () => {
   const togglePasswordVisibility = () => {
     setShowPassword(!showPassword)
   }
+
   return (
     <div className='py-4 px-6 sm:px-12 lg:px-24 xl:px-32 sm:h-full flex flex-col justify-center text-neutral-4 space-y-6'>
-      <h1 className='headline-7 sm:headline-6 lg:headline-5 xl:headline-4  text-black'>Sign Up</h1>
+      <h1 className='headline-7 sm:headline-6 lg:headline-5 xl:headline-4 text-black'>{t('title', 'Sign Up')}</h1>
       <div className='flex sm:block lg:flex space-x-1 mb-3 sm:mb-6 body-2'>
-        <span className='font-normal'>Already have an account?</span>
+        <span className='font-normal'>{t('alreadyAccount', 'Already have an account?')}</span>
         <div>
-          <Link to='/signin' className=' body-2-semi text-green'>
-            Sign In
+          <Link to='/signin' className='body-2-semi text-green'>
+            {t('signin', 'Sign In')}
           </Link>
         </div>
       </div>
@@ -57,8 +61,8 @@ const SignUp = () => {
           render={({ field }) => (
             <FormItem>
               <Input
-                className='text-black flex h-10 w-full border-b border-b-neutral-3 bg-white px-3 py-2  body-2'
-                placeholder='Your name'
+                className='text-black flex h-10 w-full border-b border-b-neutral-3 bg-white px-3 py-2 body-2'
+                placeholder={t('yournamePlaceholder', 'Your name')}
                 {...field}
               />
               <FormMessage>{form.formState.errors.yourname?.message}</FormMessage>
@@ -70,8 +74,8 @@ const SignUp = () => {
           render={({ field }) => (
             <FormItem>
               <Input
-                className='text-black flex h-10 w-full border-b border-b-neutral-3 bg-white px-3 py-2 body-2 '
-                placeholder='Your email address'
+                className='text-black flex h-10 w-full border-b border-b-neutral-3 bg-white px-3 py-2 body-2'
+                placeholder={t('emailPlaceholder', 'Your email address')}
                 {...field}
               />
               <FormMessage>{form.formState.errors.email?.message}</FormMessage>
@@ -83,8 +87,8 @@ const SignUp = () => {
           render={({ field }) => (
             <FormItem>
               <Input
-                className='text-black flex h-10 w-full border-b border-b-neutral-3 bg-white px-3 py-2 body-2 '
-                placeholder='Your username'
+                className='text-black flex h-10 w-full border-b border-b-neutral-3 bg-white px-3 py-2 body-2'
+                placeholder={t('usernamePlaceholder', 'Your username')}
                 {...field}
               />
               <FormMessage>{form.formState.errors.username?.message}</FormMessage>
@@ -98,7 +102,7 @@ const SignUp = () => {
               <Input
                 className='flex h-10 w-full border-b border-b-neutral-3 bg-white px-3 py-2 body-2'
                 type={showPassword ? 'text' : 'password'}
-                placeholder='Password'
+                placeholder={t('passwordPlaceholder', 'Password')}
                 {...field}
               />
               <button className='absolute right-3 top-[30%] -translate-y-1/2' onClick={togglePasswordVisibility}>
@@ -112,13 +116,20 @@ const SignUp = () => {
           <div className='flex leading-none lg:mb-0 space-x-2'>
             <Checkbox className='mt-[1px] sm:mt-[4px]' id='remember' {...form.register('remember')} />
             <label htmlFor='remember' className='caption-2 sm:body-2'>
-              I agree with <span className='text-black caption-2-semi sm:body-2-semi'>Privacy Policy</span> and Terms of
-              Use <span className='text-black caption-2-semi sm:body-2-semi'>Terms of Use</span>
+              {t('privacyPolicy', 'I agree with ')}
+              &nbsp;
+              <span className='text-black caption-2-semi sm:body-2-semi'>
+                {t('privacyPolicyLink', 'Privacy Policy')}
+              </span>
+              &nbsp;
+              {t('andTermsOfUse', ' and ')}
+              &nbsp;
+              <span className='text-black caption-2-semi sm:body-2-semi'>{t('termsOfUseLink', 'Terms of Use')}</span>
             </label>
           </div>
         </div>
         <Button typeof='submit' variant='auth' size='auth' type='submit'>
-          Sign Up
+          {t('signupButton', 'Sign Up')}
         </Button>
       </Form>
     </div>
