ba6586012cff0d6cb3bb3515f3faaee49dfa5781
plinh204 <nguyenphuonglinh05052004@gmail.com>
2024-10-13 21:16:03 +0700

    fix icon blog detail join new letter

--- src/pages/(site)/blog-detail/components/JoinNewsletter.tsx
+++ src/pages/(site)/blog-detail/components/JoinNewsletter.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { Mail } from 'lucide-react';
 
 const JoinNewsletter = () => {
   return (
@@ -13,9 +12,9 @@ const JoinNewsletter = () => {
         <div className="absolute inset-0 bg-white bg-opacity-70 flex items-center justify-center">
           <div className="p-8">
             <h1 className="text-2xl font-bold mb-2 text-center">Join Our Newsletter</h1>
-            <p className="text-gray-600 mb-4 text-center">Sign up for deals, new products and promotions</p>
+            <p className="text-gray-600 mb-4 text-center">Sign up for deals, new products, and promotions</p>
             <div className="flex items-center border-b border-gray-300 py-2">
-            <FontAwesomeIcon icon={faEnvelope} className="text-gray-500 mr-2" />
+              <Mail className="text-gray-500 mr-2" />
               <input 
                 className="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none" 
                 type="text" 
