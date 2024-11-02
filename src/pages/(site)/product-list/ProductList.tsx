7ddc3990b85933fc0d9f00cbe7ece2023af52c35
RiverNguyen <ngiang1309@gmail.com>
2024-11-02 23:00:02 +0700

    UPDATE: Product List page

--- src/pages/(site)/product-list/ProductList.tsx
+++ src/pages/(site)/product-list/ProductList.tsx
@@ -1,13 +1,17 @@
 import Container from '@/components/Container'
-import Banner from '../home/_components/Banner'
+import Banner from '@/pages/(site)/home/_components/Banner'
 import ProductFilter from './components/ProductFilter'
+import ProductGrid from './components/ProductGrid'
 
 const ProductList = () => {
   return (
     <>
       <Banner text={false} />
       <Container className='pt-16 pb-24'>
-        <ProductFilter />
+        <div className='flex'>
+          <ProductFilter />
+          <ProductGrid />
+        </div>
       </Container>
     </>
   )
